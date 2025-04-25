3fbee621b1704bf8f70129fad4c62b6bac5f5774
pedrote89@gmail.com <pedrotec89@gmail.com>
2025-04-25 16:05:19 -0300

    atualização da pagina

--- src/components/cardapio.jsx
+++ src/components/cardapio.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 const itens = [
-    { nome: "Bauru Simples", preco: 10.3, imagem: "/public/imagens/bauru simples.png" },
-    { nome: "Bauru com Ovo", preco: 15.0, imagem: "/public/imagens/bauru com ovos.png" },
-    { nome: "Hamburguer", preco: 7.0, imagem:"/public/imagens/hamburguer3.png" },
-    { nome: "Cheeseburguer", preco: 9.0, imagem: "/public/imagens/cheeseburg3.png" },
-    { nome: "Cachorro Quente", preco: 7.0, imagem: "/public/imagens/cachorro2.png" },
-    { nome: "Batata Frita", preco: 10.0, imagem: "/public/imagens/batata2.png" },
-    { nome: "Guaraviton", preco: 6.0, imagem: "/public/imagens/guaravitoon.png" },
-    { nome: "Refrigerante", preco: 8.0, imagem: "/public/imagens/refrigerantes 3.png" },
-    { nome: "Sanduiche Natural ", preco: 11.0, imagem: "/public/imagens/sanduiche.png" },
-    { nome: "Calabresa ", preco: 15.5, imagem: "/public/imagens/calabresa.png" },
+    { nome: "Bauru Simples", preco: 10.3, imagem: "/public/imagens/BAURU-.PNG" },
+    { nome: "Bauru com Ovo", preco: 15.0, imagem: "/public/imagens/BAURU COM OVO-.PNG" },
+    { nome: "Hamburguer", preco: 7.0, imagem:"/public/imagens/hambuer-.png" },
+    { nome: "Cheeseburguer", preco: 9.0, imagem: "/public/imagens/cheeseburg3--.png" },
+    { nome: "Cachorro Quente", preco: 7.0, imagem: "/public/imagens/cachorro-.png" },
+    { nome: "Batata Frita", preco: 10.0, imagem: "/public/imagens/FRITAS--.PNG      " },
+    { nome: "Guaraviton", preco: 6.0, imagem: "/public/imagens/Guaraviton-.PNG" },
+    { nome: "Refrigerante", preco: 8.0, imagem: "/public/imagens/REFRI--.PNG" },
+    { nome: "Sanduiche Natural ", preco: 11.0, imagem: "/public/imagens/Sanduiche Natural-.PNG" },
+    { nome: "Calabresa ", preco: 15.5, imagem: "/public/imagens/CALABRESA-.PNG" },
 ];
 
 function Cardapio(){
